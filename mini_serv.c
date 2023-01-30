25663942a7f9d2a96d6f20935db809480cb6e63a
Mehmet Eren Akbulut <makbulut@student.42istanbul.com.tr>
2023-01-30 15:08:02 +0300

    exam06 done

--- mini_serv.c
+++ mini_serv.c
@@ -66,6 +66,132 @@ t_client	*add_newclient(t_client *client_lst, int fd, int id)
 	last->next = newcli;
 	return (client_lst);
 }
+
+char	*str_join(char *s1, char *s2)
+{
+	char *join;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	join = malloc(sizeof(char) * strlen(s1) + strlen(s2) + 1);
+	if (!join)
+		return NULL;
+	while (s1[i])
+		join[j++] = s1[i++];
+	i = 0;
+	while(s2[i])
+		join[j++] = s2[i++];
+	join[j] = 0;
+	free(s1);
+	return (join);
+}
+
+ssize_t	receive_message(t_client *client_lst, t_client *curr_cli)
+{
+	ssize_t	size, ret;
+	char	buffer[1001];
+
+	size = 0;
+	while ((ret = recv(curr_cli->fd, buffer, 10000, 0)) > 0)
+	{
+		size += ret;
+		buffer[ret] = 0;
+		curr_cli->buffer = str_join(curr_cli->buffer, buffer);
+		if (!curr_cli->buffer)
+			exit_error("Fattal Error\n", client_lst);
+	}
+	if (size == 0 && ret == -1)
+		return (-1);
+	return (size);
+}
+
+t_client	*remove_client(t_client *client_lst, t_client *rmcli)
+{
+	char	str[1000];
+	t_client	*prev;
+
+	if (client_lst == rmcli)
+		client_lst = rmcli->next;
+	else
+	{
+		prev = client_lst;
+		while (prev->next != rmcli)
+			prev = prev->next;
+		prev->next = rmcli->next;
+	}
+	sprintf(str, "server: client %d, just left\n", rmcli->id);
+	send_message(str, client_lst, rmcli);
+	FD_CLR(rmcli->fd, &fd_all);
+	close(rmcli->fd);
+	free(rmcli->buffer);
+	free(rmcli);
+	return (client_lst);
+}
+
+int	extract_message(char **buf, char **msg)
+{
+	char	*newbuf;
+	int	i;
+
+	*msg = 0;
+	if (*buf == 0)
+		return (0);
+	i = 0;
+	while ((*buf)[i])
+	{
+		if ((*buf)[i] == '\n')
+		{
+			newbuf = calloc(1, sizeof (*newbuf) * (strlen(*buf + i + 1) + 1));
+			if (newbuf == 0)
+				return (-1);
+			strcpy(newbuf, *buf + i + 1);
+			*msg = *buf;
+			(*msg)[i + 1] = 0;
+			*buf = newbuf;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+t_client	*handle_client(t_client *client_lst)
+{
+	char	*msg, *str;
+	ssize_t	ret;
+	t_client	*it, *curr_cli;
+
+	it = client_lst;
+	while (it)
+	{
+		curr_cli = it;
+		it = curr_cli->next;
+		if(FD_ISSET(curr_cli->fd, &fd_rd))
+		{
+			ret = receive_message(client_lst, curr_cli);
+			if (ret == 0)
+				client_lst = remove_client(client_lst, curr_cli);
+			else if (ret > 0)
+			{
+				while ((ret = extract_message(&curr_cli->buffer, &msg)) == 1)
+				{
+					if (!(str = malloc(sizeof(char) * (20 + strlen(msg)))))
+						exit_error("Fatal Error\n", client_lst);
+					sprintf(str, "client %d: %s", curr_cli->id, msg);
+					send_message(str, client_lst, curr_cli);
+					free(msg);
+					free(str);
+				}
+				if (ret == -1)
+					exit_error("Fatal Error\n", client_lst);
+			}
+		}
+	}
+	return (client_lst);
+}
+
 void	handle_server(int sockfd)
 {
 	int	connfd, maxfd, client_fd;
@@ -91,10 +217,11 @@ void	handle_server(int sockfd)
 				++client_fd;
 			}
 		}
-		//else
-		//	client_lst = handle_client(client_lst);
+		else
+			client_lst = handle_client(client_lst);
 	}
 }
+
 int main(int ac, char **av)
 {
 	int	port, sockfd;
